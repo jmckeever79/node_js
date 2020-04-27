ae1bb48f9409c4c76f5ebec40e98eea157c84ddf
jmckeever79 <jj@jjmckeever.com>
2020-04-26 20:55:48 -0400

    examing server request

--- primo.js
+++ primo.js
@@ -4,6 +4,7 @@ var dt = require("./dtModule")
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write("Hello World, you stupid dingus!</br>");
-    res.write("The current time is "+dt.myDateTime());
+    res.write("The current time is "+dt.myDateTime()+"</br>");
+    res.write(req.url);
     res.end();
 }).listen(8080);    
