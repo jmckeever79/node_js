a109d74d276177923f791154a69c28674642bf90
jmckeever79 <jj@jjmckeever.com>
2020-04-26 20:54:16 -0400

    creating custom node.js modules

--- primo.js
+++ primo.js
@@ -1,6 +1,9 @@
 var http = require("http");
+var dt = require("./dtModule")
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.end("Hello World, you stupid dingus!");
-}).listen(8080); 
+    res.write("Hello World, you stupid dingus!</br>");
+    res.write("The current time is "+dt.myDateTime());
+    res.end();
+}).listen(8080);    
