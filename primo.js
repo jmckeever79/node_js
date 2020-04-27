ee15e27cf48d87fe03948df08f7056a40a45aa9a
jmckeever79 <jj@jjmckeever.com>
2020-04-26 21:01:36 -0400

    parsing query string

--- primo.js
+++ primo.js
@@ -1,10 +1,13 @@
 var http = require("http");
-var dt = require("./dtModule")
+var url = require("url");
+var dt = require("./dtModule");
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
     res.write("Hello World, you stupid dingus!</br>");
     res.write("The current time is "+dt.myDateTime()+"</br>");
-    res.write(req.url);
+    q = url.parse(req.url, true).query;
+    txt = q.horse+" "+q.race;
+    res.write(txt);
     res.end();
 }).listen(8080);    
