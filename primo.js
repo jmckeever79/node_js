c2c2a9a4e37e2de6e345ab00516d71c4f02a36b9
jmckeever79 <jj@jjmckeever.com>
2020-04-27 21:05:52 -0400

    playing with fs module

--- primo.js
+++ primo.js
@@ -1,4 +1,5 @@
 var http = require("http");
+var fs = require("fs");
 var url = require("url");
 var dt = require("./dtModule");
 
@@ -10,4 +11,12 @@ http.createServer(function (req, res) {
     txt = q.horse+" "+q.race;
     res.write(txt);
     res.end();
-}).listen(8080);    
+}).listen(8080);
+
+http.createServer(function (req, res) {
+    fs.readFile("frontpage.html", function (err, data) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8081);
